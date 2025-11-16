0cb65185fa8a4ed58f96f95feb1937ee4cb12881
Abhijeet <jeetabhi047@gmail.com>
2025-11-17 01:23:23 +0530

    need various improvements

--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,9 @@ import "./global.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import { ClerkProvider } from "@clerk/clerk-react";
+import { VITE_CLERK_PUBLISHABLE_KEY } from "./lib/utils.js";
 
-const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+const PUBLISHABLE_KEY = VITE_CLERK_PUBLISHABLE_KEY;
 
 if (!PUBLISHABLE_KEY) {
   throw new Error(" Missing VITE_CLERK_PUBLISHABLE_KEY in your .env file");
